56134b7e0d37a3b148c14509fbcdbaefe9acbb71
Grant <grantdamico@gmail.com>
2011-10-10 17:41:02 -0500

    First Files

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: grantdamico
+ * Date: 10/10/11
+ * Time: 4:31 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+    public static void
+}
