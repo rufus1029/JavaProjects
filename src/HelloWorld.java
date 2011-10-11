e690ff4844a7540f4ee85d1eba16310a127aef2b
Grant <grantdamico@gmail.com>
2011-10-10 20:20:41 -0500

    Update 0.1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,3 +1,5 @@
+import com.sun.servicetag.SystemEnvironment;
+
 /**
  * Created by IntelliJ IDEA.
  * User: grantdamico
@@ -6,5 +8,7 @@
  * To change this template use File | Settings | File Templates.
  */
 public class HelloWorld {
-    public static void
+    public static void main (String[] args) {
+        System.out.print("Hello World");
+    }
 }
